18ba146d4af426acd31a773083f772b3d9e461f2
Stef <18282455+LentnerStefan@users.noreply.github.com>
2024-05-15 16:59:22 +0300

    feat: set up example project with react-native-vision-camera

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,55 @@
 import * as React from 'react';
-
 import { StyleSheet, View, Text } from 'react-native';
 import { multiply } from 'react-native-vision-camera-plugin-frame-to-file';
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+  useFrameProcessor,
+} from 'react-native-vision-camera';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const device = useCameraDevice('back');
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    multiply(3, 7).then((result) => console.log(result));
   }, []);
 
+  const {
+    hasPermission: hasCameraPermission,
+    requestPermission: requestCameraPermission,
+  } = useCameraPermission();
+
+  // Triggers the opening of the device settings if the user denies the permission request.
+  const handleCameraPermissionRequest = async () => {
+    await requestCameraPermission();
+  };
+
+  const fakeFrameProcessor = useFrameProcessor((_frame) => {
+    'worklet';
+    console.log('hey');
+  }, []);
+
+  if (!hasCameraPermission) {
+    return (
+      <View
+        style={styles.container}
+        onTouchStart={handleCameraPermissionRequest}
+      >
+        <Text>Missing permission! Press here</Text>
+      </View>
+    );
+  }
+  if (!device) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
+    <View style={StyleSheet.absoluteFill}>
+      <Camera
+        style={styles.camera}
+        isActive
+        device={device}
+        frameProcessor={fakeFrameProcessor}
+      />
     </View>
   );
 }
@@ -23,9 +60,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  camera: {
+    flex: 1,
   },
 });
